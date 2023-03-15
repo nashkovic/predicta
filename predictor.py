796d146a237395924b4aa9486837eecd127e9fec
Nashkovic <117375649+nashkovic@users.noreply.github.com>
2023-03-15 03:43:16 +0200

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -1,26 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+import pandas as pd
 
-# Get team names from user input
-team1 = input("Enter Team 1 Name: ")
-team2 = input("Enter Team 2 Name: ")
+def predict_winner(team1, team2):
+    # Load the data for both teams
+    team1_data = pd.read_csv(f'data/{team1}.csv')
+    team2_data = pd.read_csv(f'data/{team2}.csv')
 
-# Search for recent form and statistics on the internet
-url = "https://www.example.com/search?q=" + team1 + "+" + team2
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+    # Calculate the recent form of each team
+    team1_form = team1_data['Result'].tail(5).value_counts(normalize=True).get('W', 0)
+    team2_form = team2_data['Result'].tail(5).value_counts(normalize=True).get('W', 0)
 
-# Analyze data to find likely winner
-# Here, we'll just assume that the team with the better recent form and statistics is the likely winner
-team1_recent_form = 7
-team1_statistics = 85
-team2_recent_form = 5
-team2_statistics = 72
+    # Calculate the overall statistics for each team
+    team1_stats = team1_data.describe().loc[['mean', 'std', 'min', 'max']].to_dict()
+    team2_stats = team2_data.describe().loc[['mean', 'std', 'min', 'max']].to_dict()
 
-if team1_recent_form + team1_statistics > team2_recent_form + team2_statistics:
-    likely_winner = team1
-else:
-    likely_winner = team2
+    # Combine the form and statistics into a single dictionary
+    team1_dict = {'form': team1_form, 'stats': team1_stats}
+    team2_dict = {'form': team2_form, 'stats': team2_stats}
 
-# Return result to user
-print("Based on recent form and statistics, the likely winner is: " + likely_winner)
+    # Determine the likely winner based on the form and statistics
+    if team1_form > team2_form:
+        winner = team1
+    elif team2_form > team1_form:
+        winner = team2
+    else:
+        if team1_stats['Goals']['mean'] > team2_stats['Goals']['mean']:
+            winner = team1
+        elif team2_stats['Goals']['mean'] > team1_stats['Goals']['mean']:
+            winner = team2
+        else:
+            winner = 'Draw'
+
+    # Return the winner as a string
+    return winner
